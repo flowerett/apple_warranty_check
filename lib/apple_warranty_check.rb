5a60cc5d8fc773486c9373b5ff08748639e91d67
Nick Chernyshev <nick.chernyshev@gmail.com>
2015-06-08 14:12:30 +0500

    parse product info

--- lib/apple_warranty_check.rb
+++ lib/apple_warranty_check.rb
@@ -1,5 +1,44 @@
 require "apple_warranty_check/version"
+require 'net/http'
 
 module AppleWarrantyCheck
-  # Your code goes here...
+  class Process
+    CHECK_URL = 'https://selfsolve.apple.com/wcResults.do'.freeze
+
+    PRODUCT_INFO_REGEXP = /warrantyPage.warrantycheck.displayProductInfo\((.*)\);/
+    PH_SUPPORT_INFO_REGEXP = /warrantyPage.warrantycheck.displayPHSupportInfo\((.*)\);/
+    HW_SUPPORT_INFO_REGEXP = /warrantyPage.warrantycheck.displayHWSupportInfo\((.*)\);/
+
+    ERR_RESP_REGEXP = /var errorMsg = '(.*)';/
+
+    attr_accessor :imei
+
+    def initialize(imei=nil)
+      @imei = imei
+    end
+
+    def run
+      parse_body get_response.body
+    end
+
+    def parse_body(html)
+      if ERR_RESP_REGEXP.match(html).nil?
+        [
+          [:prodImgUrl, :prodDesc, :isIMEINum, :APIMEINum, :isProdId, :prodId, :sn],
+          PRODUCT_INFO_REGEXP.match(html)[1].split(',').map(&:strip)
+        ].transpose.to_h
+      else
+        { error: ERR_RESP_REGEXP.match(html)[1] }
+      end
+    end
+
+    def get_response
+      uri = URI(CHECK_URL)
+      params = {sn: imei, Continue: 'Continue', cn: '', local: '', caller: '', num: 0 }
+
+      uri.query = URI.encode_www_form(params)
+
+      Net::HTTP.get_response(uri)
+    end
+  end
 end
--- spec/apple_warranty_check_spec.rb
+++ spec/apple_warranty_check_spec.rb
@@ -4,4 +4,34 @@ describe AppleWarrantyCheck do
   it 'has a version number' do
     expect(AppleWarrantyCheck::VERSION).to eq '0.0.1'
   end
+
+  describe '#parse_body' do
+    it 'returns device data from valid html response' do
+      valid_warranty_html =  File.open('spec/files/valid_warranty_resp.html').read
+
+      product_info = AppleWarrantyCheck::Process.new().parse_body valid_warranty_html
+
+      expect(product_info).to be_a Hash
+      expect(product_info[:APIMEINum]).to eq "'013977000323877'"
+      expect(product_info[:prodDesc]).to eq "'iPhone 5c'"
+    end
+
+    it 'returns device data from expired html response' do
+      expired_warranty_html =  File.open('spec/files/expired_warranty_resp.html').read
+
+      product_info = AppleWarrantyCheck::Process.new().parse_body expired_warranty_html
+
+      expect(product_info).to be_a Hash
+      expect(product_info[:APIMEINum]).to eq "'013896000639712'"
+      expect(product_info[:prodDesc]).to eq "'iPhone 5c'"
+    end
+
+    it 'returns error message from invalid html response' do
+      invalid_imei_html =  File.open('spec/files/invalid_sn_resp.html').read
+
+      product_info = AppleWarrantyCheck::Process.new().parse_body invalid_imei_html
+
+      expect(product_info[:error]).to match(/serial number you have provided cannot be found/)
+    end
+  end
 end
